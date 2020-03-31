12dc5659b673ec19b893d04b8d30107b5a6e7591
Ionut Iancu <iancu.ionut96int@gmail.com>
2020-03-31 22:46:17 +0300

    Updates

---
+++ precache-manifest.29c6ade32fb75e67441d9f1702dc27b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbae791731b892f081781d31b981daf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d5586612f90578d63026",
+    "url": "/monsters-rolodex/static/css/main.9e17ad91.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5586612f90578d63026",
+    "url": "/monsters-rolodex/static/js/main.0cbee16a.chunk.js"
+  },
+  {
+    "revision": "5186047b825f1e97286f",
+    "url": "/monsters-rolodex/static/js/runtime-main.34ec292c.js"
+  }
+]);
